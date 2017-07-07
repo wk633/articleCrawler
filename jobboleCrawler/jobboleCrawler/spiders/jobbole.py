653042d16a7c77e9e84aaa3cd836f2a0ae7f6371
wk633 <wk633@outlook.com>
2017-07-07 10:55:02 -0700

    modify __init__ file to solve import problem

--- jobboleCrawler/jobboleCrawler/spiders/jobbole.py
+++ jobboleCrawler/jobboleCrawler/spiders/jobbole.py
@@ -3,6 +3,9 @@ import scrapy
 from scrapy.http import Request
 from urllib import parse
 
+from scrapy.loader import ItemLoader
+from jobboleCrawler.items import JobbolecrawlerItem
+from jobboleCrawler.utils.common import get_md5
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
@@ -19,5 +22,13 @@ class JobboleSpider(scrapy.Spider):
 
 
     def parse_detail(self, response):
+
+
+        # load Itemloader
+        item_loader = ItemLoader(item=JobbolecrawlerItem(), response=response)
+        item_loader.add_css('title', 'div.entry-header > h1::text')
+        item_loader.add_value('url', response.url)
+
+
         pass
 
