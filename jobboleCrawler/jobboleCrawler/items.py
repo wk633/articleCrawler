7b9f90fb327f1f7c14c361b9f98c46903115695f
wk633 <wk633@outlook.com>
2017-07-07 15:39:09 -0700

    prep to connect to mysql

--- jobboleCrawler/jobboleCrawler/items.py
+++ jobboleCrawler/jobboleCrawler/items.py
@@ -6,6 +6,10 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import re
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+import datetime
+from scrapy.loader import ItemLoader
 
 
 class JobbolecrawlerItem(scrapy.Item):
@@ -13,12 +17,50 @@ class JobbolecrawlerItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
+
+# override ItemLoader
+class ArticleItemLoader(ItemLoader):
+    # custom itemloader
+    default_output_processor = TakeFirst()
+
+
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strptime(value, "%Y%m%d").date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+    return create_date
+
+
+def get_nums(value):
+    match_re = re.match(".*?(\d+).*", value);
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+    return nums
+
+def filter_tag(value):
+    if "评论" in value:
+        return ""
+    else:
+        return value
+
 class JobboleArticleItem(scrapy.Item):
     title = scrapy.Field()
     url = scrapy.Field()
     url_object_id = scrapy.Field()
-    create_date = scrapy.Field()
-    praise_nums = scrapy.Field()
-    fav_nums = scrapy.Field()
-    tag_list = scrapy.Field()
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert)
+    )
+    praise_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    fav_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    tag_list = scrapy.Field(
+        input_processor=MapCompose(filter_tag),
+        output_processor=Join(", ")
+    )
     content = scrapy.Field()
