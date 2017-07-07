313f22f73237ead7455fdae752fb6bc6c14a6a9c
wk633 <wk633@outlook.com>
2017-07-07 16:18:02 -0700

    add mysql save pipeline

--- jobboleCrawler/jobboleCrawler/pipelines.py
+++ jobboleCrawler/jobboleCrawler/pipelines.py
@@ -5,7 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import MySQLdb
 
 class JobbolecrawlerPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class MysqlPipeline(object):
+    def __init__(self):
+        self.conn = MySQLdb.connect('127.0.0.1', 'root', '', 'article_spider', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+
+        insert_sql = """
+                        insert into article (title, create_date, url, url_object_id, praise_nums, 
+                        fav_nums, tag_list, content) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql, (item['title'], item['create_date'], item['url'], item['url_object_id'],item['praise_nums'], item['fav_nums'], item['tag_list'], item['content']))
+        self.conn.commit()
--- jobboleCrawler/jobboleCrawler/settings.py
+++ jobboleCrawler/jobboleCrawler/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'jobboleCrawler.pipelines.JobbolecrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'jobboleCrawler.pipelines.JobbolecrawlerPipeline': 300,
+   'jobboleCrawler.pipelines.MysqlPipeline': 1,
+
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
