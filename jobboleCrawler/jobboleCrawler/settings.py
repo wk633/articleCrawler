a936a5445448009dbab312428d38cd853f1fbb40
wk633 <wk633@outlook.com>
2017-07-06 23:47:06 -0700

    fist setting.py bug and create python script driver

--- jobboleCrawler/jobboleCrawler/settings.py
+++ jobboleCrawler/jobboleCrawler/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'jobboleCrawler.spiders'
 #USER_AGENT = 'jobboleCrawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = false
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
