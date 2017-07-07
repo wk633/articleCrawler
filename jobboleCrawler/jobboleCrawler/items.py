92f32996d28ba8cb5f7d95965d3b858fce8c93dd
wk633 <wk633@outlook.com>
2017-07-07 11:29:21 -0700

    add item_loader rules and define item

--- jobboleCrawler/jobboleCrawler/items.py
+++ jobboleCrawler/jobboleCrawler/items.py
@@ -12,3 +12,13 @@ class JobbolecrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class JobboleArticleItem(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    create_date = scrapy.Field()
+    praise_nums = scrapy.Field()
+    fav_nums = scrapy.Field()
+    tag_list = scrapy.Field()
+    content = scrapy.Field()
--- jobboleCrawler/jobboleCrawler/spiders/jobbole.py
+++ jobboleCrawler/jobboleCrawler/spiders/jobbole.py
@@ -4,7 +4,7 @@ from scrapy.http import Request
 from urllib import parse
 
 from scrapy.loader import ItemLoader
-from jobboleCrawler.items import JobbolecrawlerItem
+from jobboleCrawler.items import JobboleArticleItem
 from jobboleCrawler.utils.common import get_md5
 
 class JobboleSpider(scrapy.Spider):
@@ -18,17 +18,23 @@ class JobboleSpider(scrapy.Spider):
         for post_node in post_nodes:
             img_url = post_node.css("img::attr(href)").extract_first("");
             post_url = post_node.css("::attr(href)").extract_first("");
-            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parseDetail)
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
 
 
     def parse_detail(self, response):
 
-
         # load Itemloader
-        item_loader = ItemLoader(item=JobbolecrawlerItem(), response=response)
-        item_loader.add_css('title', 'div.entry-header > h1::text')
-        item_loader.add_value('url', response.url)
+        item_loader = ItemLoader(item=JobboleArticleItem(), response=response)
+        item_loader.add_css("title", ".entry-header h1::text")
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("url_object_id", get_md5(response.url))
+        item_loader.add_css("create_date", "p.entry-meta-hide-on-mobile::text")
+        item_loader.add_css("praise_nums", "vote-post-up  h10::text")
+        item_loader.add_css("fav_nums", ".bookmark-btn::text")
+        item_loader.add_css("tag_list", "p.entry-meta-hide-on-mobile a::text")
+        item_loader.add_css("content", "div.entry")
 
+        article_item = item_loader.load_item()
 
-        pass
+        yield article_item
 
